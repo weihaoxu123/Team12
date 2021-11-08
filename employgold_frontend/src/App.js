621bf53bcbd556b3e87afcfa104c52be50438071
Yingjie Shen <shen92@wisc.edu>
2021-11-07 23:52:06 -0800

    configure routes

--- employgold_frontend/src/App.js
+++ employgold_frontend/src/App.js
@@ -1,24 +1,23 @@
-import logo from "./logo.svg";
-import "./styles/App.scss";
+import React from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Button from "@mui/material/Button";
 
+import { routes } from "./views/routes";
+import { Box } from "@mui/system";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        {routes.map((route) => (
+          <Route
+            key={route.name}
+            exact={route.exact}
+            path={route.path}
+            element={route.element}
+          />
+        ))}
+      </Routes>
+    </BrowserRouter>
   );
 }
 
