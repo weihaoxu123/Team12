76efdc9f0bba4fe804ee6301e9029e89d0ca29f3
Yingjie Shen <shen92@wisc.edu>
2021-11-11 01:51:15 -0800

    add not found

--- employgold_frontend/src/views/NotFound.tsx
+++ employgold_frontend/src/views/NotFound.tsx
@@ -1,21 +1,32 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import { Box } from '@mui/system';
 import { Button } from '@mui/material';
-import { useNavigate } from 'react-router-dom';
 
+import NavBar from 'src/components/NavBar';
 export default function NotFound() {
   const navigate = useNavigate();
   return (
     <Box
       sx={{
-        height: '100vh',
+        minHeight: '100vh',
         display: 'flex',
         flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
       }}>
-      <Box>Page Not Found</Box>
-      <Button onClick={() => navigate(-1)}>Go Back</Button>
+      <NavBar />
+      <Box
+        sx={{
+          flex: 1,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}>
+        <Box sx={{ typography: 'h4' }} mb={6}>
+          Page Not Found
+        </Box>
+        <Button onClick={() => navigate(-1)}>Go Back</Button>
+      </Box>
     </Box>
   );
 }
