a143abdf49c3fa6742634420fcca58f93926da9a
Yifei Li <liy12@rose-hulman.edu>
2021-11-25 04:01:06 -0800

    types and preference page update

--- employgold_frontend/src/components/CandidateProfileEditComponents/CandidateJobPreferencesSection.tsx
+++ employgold_frontend/src/components/CandidateProfileEditComponents/CandidateJobPreferencesSection.tsx
@@ -21,14 +21,49 @@ interface ICandidateJobPreferenceSectionProps {
   handleBackClick: () => void;
 }
 
+const defaultGeneralMatchingPreference: IGeneralMatchingPreference = {
+  type: [],
+  size: [],
+  revenue: [],
+  financialStage: [],
+  compensationAndBenefits: [],
+  techStack: [],
+  otherTechStack: [],
+  mission: [],
+  diversityEquityInclusionBelonging: [],
+  culture: [],
+  assessments: [],
+  organizationStructure: [],
+  schedule: [],
+  location: [],
+};
+
 const defaultJobPreferenceInfo: IJobPreferenceInfo = {
-  annualSalary: '',
-  jobReportToCEO: false,
-  continuousImprovementCulture: false,
-  workLocation: [],
-  authorizationToWorkUS: false,
-  visaSponsorship: false,
-  femaleDiversity: false,
+  jobType: '',
+  jobCompensationType: '',
+  minimumBaseCompensation: {},
+  lowEndCompensation: {},
+  highEndCompensation: {},
+  lastCompensationPackage: { targetBonus: 0, actualBonus: 0 },
+  retirementPlanPreference: { has: false },
+  medicalHealthcareBenefitPlanPreference: { has: false },
+  dentalCareBenefitPlanPreference: { has: false },
+  visionCareBenefitPlanPreference: { has: false },
+  vacationSickDaysHolidayPTOPreference: {
+    vacations: 0,
+    sickDays: 0,
+    holidays: 0,
+  },
+  spendingAllowancePreference: { has: false },
+  tuitionBenefitPreference: { has: false },
+  companyVehiclePreference: {
+    hasCompanyVehicle: false,
+    hasPersonalUseCompanyVehicle: false,
+    hasVehicleAllowance: false,
+  },
+  otherBenefitsAndPerks: '',
+  nonNegotiableGeneralPreference: { ...defaultGeneralMatchingPreference },
+  negotiableGeneralPreference: { ...defaultGeneralMatchingPreference },
 };
 
 const CandidateJobPreferenceSection: React.FC<ICandidateJobPreferenceSectionProps> =
@@ -36,79 +71,6 @@ const CandidateJobPreferenceSection: React.FC<ICandidateJobPreferenceSectionProp
     const [jobPreferenceInfo, setJobPreferenceInfo] = useState(
       props.jobPreferenceInfo || defaultJobPreferenceInfo,
     );
-
-    const handleAnnualSalaryChange = (event: ChangeEvent<HTMLInputElement>) => {
-      jobPreferenceInfo.annualSalary = event.target.value;
-      setJobPreferenceInfo({
-        ...jobPreferenceInfo,
-      });
-    };
-
-    const hanldeJobLocationEnter: KeyboardEventHandler<HTMLInputElement> = (
-      event,
-    ) => {
-      if (event.key === 'Enter') {
-        const inputEle = event.target as HTMLInputElement;
-        const value = inputEle.value;
-        inputEle.value = '';
-        jobPreferenceInfo.workLocation.push(value);
-        setJobPreferenceInfo({
-          ...jobPreferenceInfo,
-        });
-      }
-    };
-
-    const handleChipDelete = (idx: number) => (event: any) => {
-      jobPreferenceInfo.workLocation.splice(idx, 1);
-      setJobPreferenceInfo({
-        ...jobPreferenceInfo,
-      });
-    };
-
-    const handleReportToCEOChange = (event: ChangeEvent<HTMLInputElement>) => {
-      jobPreferenceInfo.jobReportToCEO = !jobPreferenceInfo.jobReportToCEO;
-      setJobPreferenceInfo({
-        ...jobPreferenceInfo,
-      });
-    };
-
-    const handleContinuousImprovementCultureChange = (
-      event: ChangeEvent<HTMLInputElement>,
-    ) => {
-      jobPreferenceInfo.continuousImprovementCulture =
-        !jobPreferenceInfo.continuousImprovementCulture;
-      setJobPreferenceInfo({
-        ...jobPreferenceInfo,
-      });
-    };
-
-    const handleAuthorizationToWorkUSChange = (
-      event: ChangeEvent<HTMLInputElement>,
-    ) => {
-      jobPreferenceInfo.authorizationToWorkUS =
-        !jobPreferenceInfo.authorizationToWorkUS;
-      setJobPreferenceInfo({
-        ...jobPreferenceInfo,
-      });
-    };
-
-    const handleVisaSponsorshipChange = (
-      event: ChangeEvent<HTMLInputElement>,
-    ) => {
-      jobPreferenceInfo.visaSponsorship = !jobPreferenceInfo.visaSponsorship;
-      setJobPreferenceInfo({
-        ...jobPreferenceInfo,
-      });
-    };
-
-    const handleFemaleDiversityChange = (
-      event: ChangeEvent<HTMLInputElement>,
-    ) => {
-      jobPreferenceInfo.femaleDiversity = !jobPreferenceInfo.femaleDiversity;
-      setJobPreferenceInfo({
-        ...jobPreferenceInfo,
-      });
-    };
 
     return (
       <Box
@@ -150,177 +112,7 @@ const CandidateJobPreferenceSection: React.FC<ICandidateJobPreferenceSectionProp
               display: 'flex',
               flexDirection: 'column',
               alignItems: 'center',
-            }}>
-            <TextField
-              className="textBox"
-              type={'number'}
-              onChange={handleAnnualSalaryChange}
-              value={jobPreferenceInfo.annualSalary}
-              label="Desired Annual Salary (in K)"
-            />
-
-            <Box
-              sx={{
-                width: '300px',
-                mt: 4,
-                typography: 'body1',
-              }}>
-              Enter Your Desired Job Locations
-              <TextField
-                className="textBox"
-                sx={{ mt: 1 }}
-                label={`Press 'Enter' for next location `}
-                onKeyDown={hanldeJobLocationEnter}
-              />
-              <Paper
-                variant="outlined"
-                sx={{
-                  mt: 2,
-                  minHeight: '50px',
-                  display: 'flex',
-                  flexWrap: 'wrap',
-                }}>
-                {jobPreferenceInfo.workLocation.map((v, idx) => {
-                  return (
-                    <ListItem
-                      key={`chip-item-${idx}`}
-                      sx={{ width: 'fit-content' }}>
-                      <Chip
-                        onDelete={handleChipDelete(idx)}
-                        key={`chip-key-${idx}`}
-                        label={v}
-                      />
-                    </ListItem>
-                  );
-                })}
-              </Paper>
-            </Box>
-
-            <FormControl
-              sx={{
-                width: '100%',
-                mt: 4,
-                typography: 'body1',
-              }}>
-              <FormLabel>Want Job Reports To CEO?</FormLabel>
-              <RadioGroup onChange={handleReportToCEOChange} row>
-                <FormControlLabel
-                  control={<Radio checked={jobPreferenceInfo.jobReportToCEO} />}
-                  label="Yes"
-                />
-                <FormControlLabel
-                  control={
-                    <Radio checked={!jobPreferenceInfo.jobReportToCEO} />
-                  }
-                  label="No"
-                />
-              </RadioGroup>
-            </FormControl>
-
-            <FormControl
-              sx={{
-                width: '100%',
-                mt: 4,
-                typography: 'body1',
-              }}>
-              <FormLabel>
-                Want Company Has a Continuous Improvement Culture?
-              </FormLabel>
-              <RadioGroup
-                onChange={handleContinuousImprovementCultureChange}
-                row>
-                <FormControlLabel
-                  control={
-                    <Radio
-                      checked={jobPreferenceInfo.continuousImprovementCulture}
-                    />
-                  }
-                  label="Yes"
-                />
-                <FormControlLabel
-                  control={
-                    <Radio
-                      checked={!jobPreferenceInfo.continuousImprovementCulture}
-                    />
-                  }
-                  label="No"
-                />
-              </RadioGroup>
-            </FormControl>
-
-            <FormControl
-              sx={{
-                width: '100%',
-                mt: 4,
-                typography: 'body1',
-              }}>
-              <FormLabel>
-                Do You Have a Valid Authorization to Work in U.S?
-              </FormLabel>
-              <RadioGroup onChange={handleAuthorizationToWorkUSChange} row>
-                <FormControlLabel
-                  control={
-                    <Radio checked={jobPreferenceInfo.authorizationToWorkUS} />
-                  }
-                  label="Yes"
-                />
-                <FormControlLabel
-                  control={
-                    <Radio checked={!jobPreferenceInfo.authorizationToWorkUS} />
-                  }
-                  label="No"
-                />
-              </RadioGroup>
-            </FormControl>
-
-            <FormControl
-              sx={{
-                width: '100%',
-                mt: 4,
-                typography: 'body1',
-              }}>
-              <FormLabel>
-                Do You Require a Visa Sponsorship in The Future?
-              </FormLabel>
-              <RadioGroup onChange={handleVisaSponsorshipChange} row>
-                <FormControlLabel
-                  control={
-                    <Radio checked={jobPreferenceInfo.visaSponsorship} />
-                  }
-                  label="Yes"
-                />
-                <FormControlLabel
-                  control={
-                    <Radio checked={!jobPreferenceInfo.visaSponsorship} />
-                  }
-                  label="No"
-                />
-              </RadioGroup>
-            </FormControl>
-
-            <FormControl
-              sx={{
-                width: '100%',
-                mt: 4,
-                typography: 'body1',
-              }}>
-              <FormLabel>Do You Want a Female Diversity?</FormLabel>
-              <RadioGroup onChange={handleFemaleDiversityChange} row>
-                <FormControlLabel
-                  control={
-                    <Radio checked={jobPreferenceInfo.femaleDiversity} />
-                  }
-                  label="Yes"
-                />
-                <FormControlLabel
-                  control={
-                    <Radio checked={!jobPreferenceInfo.femaleDiversity} />
-                  }
-                  label="No"
-                />
-              </RadioGroup>
-            </FormControl>
-          </Box>
+            }}></Box>
 
           <Button
             className="button"
@@ -341,6 +133,7 @@ const CandidateJobPreferenceSection: React.FC<ICandidateJobPreferenceSectionProp
             }}>
             Save
           </Button>
+
           <Button
             className="button"
             onClick={props.handleBackClick}
--- employgold_frontend/src/types/MatchParamTypes.d.ts
+++ employgold_frontend/src/types/MatchParamTypes.d.ts
@@ -1,11 +1,110 @@
 interface IJobPreferenceInfo {
-  annualSalary: string;
-  jobReportToCEO: boolean;
-  continuousImprovementCulture: boolean;
-  workLocation: string[];
-  authorizationToWorkUS: boolean;
-  visaSponsorship: boolean;
-  femaleDiversity: boolean;
+  jobType: string;
+  jobCompensationType: string;
+  minimumBaseCompensation: ICompensation;
+  lowEndCompensation: ICompensation;
+  highEndCompensation: ICompensation;
+  lastCompensationPackage: ICompensationPackage;
+  retirementPlanPreference: IRetirementPlanPreference;
+  medicalHealthcareBenefitPlanPreference: IMedicalHealthcareBenefitPlanPreference;
+  dentalCareBenefitPlanPreference: IDentalCareBenefitPlanPreference;
+  visionCareBenefitPlanPreference: IVisionCareBenefitPlanPreference;
+  vacationSickDaysHolidayPTOPreference: IVacationSickDaysHolidayPTOPreference;
+  spendingAllowancePreference: ISpendingAllowancePreference;
+  tuitionBenefitPreference: ITuitionBenefitPreference;
+  companyVehiclePreference: ICompanyVehiclePreference;
+  otherBenefitsAndPerks?: string;
+  nonNegotiableGeneralPreference: IGeneralMatchingPreference;
+  negotiableGeneralPreference: IGeneralMatchingPreference;
+}
+
+interface ICompensation {
+  salary?: number;
+  hourly?: number;
+}
+
+interface ICompensationPackage {
+  hourly?: number;
+  salary?: number;
+  projectGigRate?: number;
+  commission?: number;
+  targetBonus: number;
+  actualBonus: number;
+}
+
+interface IRetirementPlanPreference {
+  has: boolean;
+  contributeOrMatch?: boolean;
+  retirementMatchContribution?: number;
+  comments?: string;
+}
+
+interface IMedicalHealthcareBenefitPlanPreference {
+  has: boolean;
+  contributeOrMatch?: boolean;
+  amount?: number;
+  candidateContributeAmount?: string;
+  comments?: string;
+}
+
+interface IDentalCareBenefitPlanPreference {
+  has: boolean;
+  contributeOrMatch?: boolean;
+  amount?: number;
+  candidateContributeAmount?: string;
+  comments?: string;
+}
+
+interface IVisionCareBenefitPlanPreference {
+  has: boolean;
+  contributeOrMatch?: boolean;
+  amount?: number;
+  candidateContributeAmount?: string;
+  comments?: string;
+}
+
+interface IVacationSickDaysHolidayPTOPreference {
+  vacations: number;
+  sickDays: number;
+  holidays: number;
+  PTO?: number;
+}
+
+interface ISpendingAllowancePreference {
+  has: boolean;
+  amount?: number;
+  rationale?: string;
+  comments?: string;
+}
+
+interface ITuitionBenefitPreference {
+  has: boolean;
+  amount?: number;
+}
+
+interface ICompanyVehiclePreference {
+  hasCompanyVehicle: boolean;
+  hasPersonalUseCompanyVehicle: boolean;
+  personalUseCompanyVehicleComments?: string;
+  hasVehicleAllowance: boolean;
+  vehicleAllowanceAmount?: number;
+}
+
+interface IGeneralMatchingPreference {
+  type: string[];
+  size: string[];
+  revenue: string[];
+  financialStage: string[];
+  compensationAndBenefits: string[];
+  techStack: string[];
+  otherTechStack: string[];
+  mission: string[];
+  diversityEquityInclusionBelonging: string[];
+  culture: string[];
+  assessments: string[];
+  organizationStructure: string[];
+  schedule: string[];
+  location: string[];
 }
 
 interface ICareerDevAssessmentsInfo {
